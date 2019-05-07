f2a70b70f26ea6f7eac3d811641b3d57829793a6
Sami Sabir-Idrissi <admin@techteksolution.com>
2019-05-07 04:28:57 -0400

    will try to create folders and excel inside git project

--- src/resources/java/Features/CreateResultsFolder.feature
+++ src/resources/java/Features/CreateResultsFolder.feature
@@ -4,10 +4,19 @@ Feature: create Results folder and write to excel
 	basically i want to create a folder, read and write to excel
 	because im havin issues with Jenkins 
 	im getting permission denied
+	you will get access denied when you try to access directories in your machine
+	but this time i will try to do it inside the project
 
-Scenario: create folder write to excel
+@outsideProject
+Scenario: create folder write to excel inside the project
 
 	Given i create a results folder and write to excel
+	
+	
+@insideProject
+Scenario: create folder write to excel inside the project
+
+	Given i create a results folder and write to excel inside project
 	
 	
 	
--- src/test/java/Steps/StepDefinitions.java
+++ src/test/java/Steps/StepDefinitions.java
@@ -8,12 +8,10 @@ import cucumber.api.java.en.Given;
 
 public class StepDefinitions {
 
-	
 	@Given("^i create a results folder and write to excel$")
 	public void createFoldlerAndWriteToExcel() throws IOException {
-		
-		
-		//try to read excel file
+
+		// try to read excel file
 		String projpath = "/Users/sami/Desktop/Automation/sidrissi/CucumberJenkins/Runs/";
 
 		// create reqresAPI results folder
@@ -22,38 +20,57 @@ public class StepDefinitions {
 		// create log folder
 		String logFolderPath = Lib.createLogFolder(runsFolderPath);
 
-		
 		String logfolder = logFolderPath + "/ScratchPaper";
 		String loginLogFolderPath = Lib.createLogFolder_for_Service(logfolder, "scratchPaper");
 		System.out.println("Login Log folder path : " + loginLogFolderPath);
 
 		// create excel sheet with columns
-		Constants.Constants.excelResultsPath = runsFolderPath+"/RunResults.xlsx";
-		
+		Constants.Constants.excelResultsPath = runsFolderPath + "/RunResults.xlsx";
+
 		Lib.create_SetupExcelResultsSheet(Constants.Constants.excelResultsPath);
-		
-		
-		
-		
+
 	}
-	
-	
-	
+
 	@Given("^i read a cell from a test data sheet$")
 	public void readExcelSheetThroughJenkins() throws IOException {
-		
-		
-		String excelFilePath = "/Users/sami/eclipse-workspace/JenkinsCucumber/src/resources/java/testdata/TestData.xlsx";
-		
-		String value = Lib.readexcelvalue(1, 1,excelFilePath);
-		
-		System.out.println("excelValue : "+ value);
-		
-	}
-	
-	
-	public static String createRunResultFolder(String runsFolderPath) throws IOException {
 
+		String excelFilePath = "/Users/sami/eclipse-workspace/JenkinsCucumber/src/resources/java/testdata/TestData.xlsx";
+
+		String value = Lib.readexcelvalue(1, 1, excelFilePath);
+
+		System.out.println("excelValue : " + value);
+
+	}
+
+	// i create a results folder and write to excel inside project
+	@Given("^i create a results folder and write to excel inside project$")
+	public void createFoldlerAndWriteToExcel_InsideProject() throws IOException {
+
+		// try to read excel file
+		String projpath = "/Users/sami/eclipse-workspace/JenkinsCucumber/target/ssidrissi/Runs/";
+
+		// create reqresAPI results folder
+		String runsFolderPath = createRunResultFolder(projpath);
+
+		// create log folder
+		String logFolderPath = Lib.createLogFolder(runsFolderPath);
+
+		String logfolder = logFolderPath + "/ScratchPaper";
+		String loginLogFolderPath = Lib.createLogFolder_for_Service(logfolder, "scratchPaper");
+		System.out.println("Login Log folder path : " + loginLogFolderPath);
+
+		// create excel sheet with columns
+		Constants.Constants.excelResultsPath = runsFolderPath + "/RunResults.xlsx";
+
+		Lib.create_SetupExcelResultsSheet(Constants.Constants.excelResultsPath);
+
+		String value = Lib.readexcelvalue(0, 0, Constants.Constants.excelResultsPath);
+
+		System.out.println("Excel column value 0, 0 : "+value);
+	
+	}
+
+	public static String createRunResultFolder(String runsFolderPath) throws IOException {
 
 		File dir = new File(runsFolderPath + Lib.getcurrentdatefolder());
 
@@ -61,6 +78,5 @@ public class StepDefinitions {
 
 		return dir.getAbsolutePath();
 	}
-	
-	
+
 }
